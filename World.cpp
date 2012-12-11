9a604a61654cd7c075ac2228bdf5e234db6ab0b9
pantjohn <pantazis@ceid.upatras.gr>
2012-12-11 21:47:13 +0200

    World.cpp w/ comments

--- World.cpp
+++ World.cpp
@@ -3,19 +3,19 @@
 using std::cout;
 
 struct Point{
-int posX; 
-int posY; 
-bool hasFire; 
-bool hasVehicle;
-bool hasBase;
+int posX; //x-coordinate
+int posY;  //y-coordinate
+bool hasFire; //spot is on fire
+bool hasVehicle; //vehicle on spot
+bool hasBase; //base on this spot (only for 0,0)
 }pointMap[50][50];
 
 World::World(){} //constructor
 
-void Console::run(){} //body?
+void Console::run(){} //mallon feugei opws auth ths Console
 
-void World::gettingStarted(){
-     for(int i=0; i<50; i++){
+void World::gettingStarted(){ 
+     for(int i=0; i<50; i++){  ////map creation for the first time, each spot is given default options + na allax8ei to void
              for(int j=0; j<50; j++){
                      pointMap[i][j].posX=i;
                      pointMap[i][j].posY=j;
@@ -27,9 +27,9 @@ void World::gettingStarted(){
                      
      for(int i=0; i<50; i++){
              for(int j=0; j<50; j++){
-                     if(i==0&&j==0) cout<<"BASE";                         
+                     if(i==0&&j==0) cout<<"BASE";  //show base at (0,0)                       
                      else cout<<" ";}           
                      }
-     return pointMap[50][50];
+     return pointMap[50][50]; //returns map
 }
      
