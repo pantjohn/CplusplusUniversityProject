ea5b81cae21e13bcc346e90a9019f5cf84de45ee
pantjohn <pantazis@ceid.upatras.gr>
2012-12-11 18:22:34 +0200

    World.h

---
+++ World.h
@@ -0,0 +1,14 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+class World{
+      public:
+             World();
+             run();
+             
+      private:     
+             void gettingStarted();
+             
+};
+
+#endif      
