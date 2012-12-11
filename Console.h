90b300683888c40856d1aed4c286a339a490c7f9
pantjohn <pantazis@ceid.upatras.gr>
2012-12-11 21:40:58 +0200

    Console.h w/ comments

--- Console.h
+++ Console.h
@@ -1,24 +1,24 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
-//#include den-kserw-kai-egw-poies klaseis
+
 class Vehicle;
 
 class Console{
 	public:
-		Console();
-		void run();
+		Console(); //constructor w/ no parameters
+		void run(); //o,ti isxuei apo Console.cpp
 	private:
-		int choice;
-		int createChoice; //pithanotat autes oi duo times int na mhn xreiazontai kan
+		int choice; //choice of Main Menu
+		int createChoice; //choice of Vehicle Menu
 
 		//dhlwsh kalsewn se auto to shmeio; an nai, poiwn;
 
-		void actionChoice();
-		int displayMainMenu() const;
-		int displayVehicleMenu() const;
+		void actionChoice(); //users choices
+		int displayMainMenu() const; //display of Main Menu
+		int displayVehicleMenu() const; //display of Vehicle Menu (if choosen)
 
-		Vehicle *createObject( int );
+		Vehicle *createObject( int ); //creates Vehicle
 };
 
 #endif
